7176bc966fcaa3b51a7c66b237ed52fdbeeac16b
Mohsin Ali <106370663+mohsinali2023@users.noreply.github.com>
2026-05-16 19:12:28 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+let name = "Mohsin";
+let repoCount = 50;
+
+// console.log(name  + repoCount);
+// console.log(`Hello, my name is ${name} and my repo acount is ${repoCount}`);
+
+const gameName = "Mohsin-ma-com";
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('m'));
+
+
+const newString  = gameName.substring(7,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
+
+const newStringOne = "    Mohsin     ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url  = "https://www.mohsinali%20portfolio.com";
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("mohsin"));
+
+console.log(gameName.split("-"));
+
+
+
